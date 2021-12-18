c86bed24d8bae674ea1232b8bc81ac8874b742b8
Tomáš Rončák <38561069+TomasRoncak@users.noreply.github.com>
2021-12-18 22:10:42 +0100

    kidnapper spawn, level 1 prefab

--- Assets/Scripts/NPC/Kidnapper/KidnapperSpawn.cs
+++ Assets/Scripts/NPC/Kidnapper/KidnapperSpawn.cs
@@ -3,37 +3,24 @@ using UnityEngine;
 
 public class KidnapperSpawn : MonoBehaviour
 {
+    public GameObject kidnapperPrefab;
     private GameObject kidnapper;
-    private bool triggered = false;
-
-    private void Awake()
-    {
-        kidnapper = GameObject.Find("Kidnapper");
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            triggered = true;
+            GameObject spawnPoint = new List<GameObject>(GameObject.FindGameObjectsWithTag("SpawnPoint"))
+                .Find(g => g.transform.IsChildOf(transform));
+            kidnapperPrefab.transform.position = spawnPoint.transform.position;
+
+            kidnapper = Instantiate(kidnapperPrefab, spawnPoint.transform.position, Quaternion.identity);
+            kidnapper.GetComponent<KidnapperController>().targetPlayer = other.gameObject.transform;
         }
     }
 
     private void OnTriggerExit()
     {
-        triggered = false;
-        kidnapper.SetActive(false); //if player gets out of trigger space, the kidnapper will dissapear
+        Destroy(kidnapper);
     }
-
-    void Update()
-    {
-        if (triggered && !kidnapper.activeInHierarchy && !GameManager.Instance.playerIsHide)
-        {
-            GameObject spawnPoint = new List<GameObject>(GameObject.FindGameObjectsWithTag("SpawnPoint"))
-                .Find(g => g.transform.IsChildOf(transform));
-            kidnapper.transform.position = spawnPoint.transform.position;
-            kidnapper.SetActive(true);
-        }
-    }
-
 }
