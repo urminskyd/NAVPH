a79d0adaad5826000832edcb197c4a5c9a6b12f9
Tomáš Rončák <38561069+TomasRoncak@users.noreply.github.com>
2021-12-19 09:52:08 +0100

    camera change again

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,47 +3,66 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public Transform levelView;
+
     public float sensitivity;
     public float smoothFactor = 0.5f;
-    public bool isVerticalRotationEnabled = false;
+    public bool isViewEnabled = false;
+
+    public float distance = 10.0f;
+    public float height = 5.0f; // the height we want the camera to be above the target
 
     private Camera cam;
     private float scroll;
-    private Vector3 cameraOffset;
-    private Quaternion camRotation;
 
     void Start()
     {
-        camRotation = transform.rotation;
-        camRotation.x = 20f;
         cam = Camera.main;
-        cameraOffset = transform.position - target.transform.position;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
-            isVerticalRotationEnabled = !isVerticalRotationEnabled;
+            isViewEnabled = !isViewEnabled;
 
-        scroll = Input.GetAxis("Mouse ScrollWheel") * sensitivity;
-        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView + scroll, 30, 150);
+        if (!isViewEnabled)
+        {
+            scroll = Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView + scroll, 30, 100);
+        }
     }
 
     void LateUpdate()
     {
-        camRotation.y += Input.GetAxis("Mouse X") * sensitivity;
-        camRotation.y = Mathf.Clamp(camRotation.y, -90, 90);
-
-        if (isVerticalRotationEnabled)
+        if (isViewEnabled)
         {
-            camRotation.x += Input.GetAxis("Mouse Y") * (sensitivity * 1.5f) * (-1);
-            camRotation.x = Mathf.Clamp(camRotation.x, -45, 75);
+            transform.position = levelView.transform.position + new Vector3(0, 70, 0);
+            Vector3 eulerAngles = transform.eulerAngles;
+            eulerAngles.x = 90;
+            transform.eulerAngles = eulerAngles;
         }
+        else
+        {
+            // Calculate the current rotation angles
+            float wantedRotationAngle = target.eulerAngles.y;
+            float wantedHeight = target.position.y + height;
 
-        Vector3 newPos = target.transform.position + cameraOffset;
+            float currentRotationAngle = transform.eulerAngles.y;
+            float currentHeight = transform.position.y;
 
-        transform.rotation = Quaternion.Euler(camRotation.x, camRotation.y, camRotation.z);
-        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
-        //transform.LookAt(target);
-    } 
+            currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, 3.0f * Time.deltaTime); // Damp the rotation around the y-axis
+            currentHeight = Mathf.Lerp(currentHeight, wantedHeight, 2.0f * Time.deltaTime); // Damp the height
+
+            var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0); // Convert the angle into a rotation
+
+            // Set the position of the camera on the x-z plane to:
+            // distance meters behind the target
+            transform.position = target.position;
+            transform.position -= currentRotation * Vector3.forward * distance;
+
+            // Set the height of the camera
+            transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+            transform.LookAt(target);
+        }
+    }
 }
